b3ad8fe6b5bad92c4625b9dde2674347416c4078
egrao_portatil <egrao@cifpfbmoll.eu>
2021-11-26 20:05:16 +0100

    date time picker and alert

--- app/src/main/java/com/example/task41/MainActivity.java
+++ app/src/main/java/com/example/task41/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.task41;
 
+import android.content.DialogInterface;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -63,4 +66,59 @@ public class MainActivity extends AppCompatActivity implements
     public void onNothingSelected(AdapterView<?> adapterView) {
         Log.d(TAG, "onNothingSelected: ");
     }
+
+    public void onClickShowAlert(View view) {
+        AlertDialog.Builder myAlertBuilder = new AlertDialog.Builder(MainActivity.this);
+        myAlertBuilder.setTitle(R.string.alert_title);
+        myAlertBuilder.setMessage(R.string.alert_message);
+        // Add the buttons.
+        myAlertBuilder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), R.string.pressed_ok,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        myAlertBuilder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                // User clicked the CANCEL button.
+                Toast.makeText(getApplicationContext(), R.string.pressed_cancel,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Create and show the AlertDialog.
+        myAlertBuilder.show();
+
+    }
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(),
+                getString(R.string.date_picker));
+    }
+
+    public void showTimePickerDialog(View view) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(),
+                getString(R.string.time_picker));
+    }
+
+    public void processDatePickerResult(int year, int month, int day) {
+        String month_string = Integer.toString(month + 1);
+        String day_string = Integer.toString(day);
+        String year_string = Integer.toString(year);
+        // Assign the concatenated strings to dateMessage.
+        String dateMessage = (month_string + "/" +
+                day_string + "/" + year_string);
+        Toast.makeText(this, getString(R.string.date) + dateMessage,
+                Toast.LENGTH_SHORT).show();
+    }
+    public void processTimePickerResult(int hourOfDay, int minute) {
+        // Convert time elements into strings.
+        String hour_string = Integer.toString(hourOfDay);
+        String minute_string = Integer.toString(minute);
+        // Assign the concatenated strings to timeMessage.
+        String timeMessage = (hour_string + ":" + minute_string);
+        Toast.makeText(this, getString(R.string.time) + timeMessage, Toast.LENGTH_SHORT).show();
+    }
+
 }
